3a112516d312bb565f71136768af614b1d7c2c8e
Lex Sherman <alexandershershakov@gmail.com>
2024-07-12 13:41:07 +0200

    go to acEntrada

--- main.py
+++ main.py
@@ -24,19 +24,40 @@ try:
     accept_button.click()
     
     # Wait for the next page to load
-    wait.until(EC.url_contains("icpplustieb/citar?p=8&locale=es"))
+    wait.until(EC.url_contains("icpplustieb/citar"))
     
-    # Wait for the second dropdown menu to be present
-    select_sede_element = wait.until(EC.presence_of_element_located((By.ID, "sede")))
-    
-    # Print all available options to verify the text
+    # Wait for the second dropdown menu to be present and select the option
+    select_sede_element = wait.until(EC.presence_of_element_located((By.NAME, "sede")))
     select_sede = Select(select_sede_element)
-    options = [option.text for option in select_sede.options]
-    print("Available options:", options)
+    select_sede.select_by_visible_text("CNP MALLORCA GRANADOS, MALLORCA, 213")
     
-    # Select the desired option
-    select_sede.select_by_visible_text("CNP MALLORCA GRANADOS, MALLORCA")
+    # Wait for the new elements to load after selecting the office
+    wait.until(EC.presence_of_element_located((By.ID, "tramiteGrupo[0]")))
     
+    # Locate the new dropdown and select the desired option
+    select_tramite_element = wait.until(EC.presence_of_element_located((By.ID, "tramiteGrupo[0]")))
+    select_tramite = Select(select_tramite_element)
+    
+    select_tramite.select_by_visible_text("POLICIA - RECOGIDA DE TARJETA DE IDENTIDAD DE EXTRANJERO (TIE)")
+    
+    # Click on the second "Aceptar" button
+    second_accept_button = wait.until(EC.presence_of_element_located((By.ID, "btnAceptar")))
+    second_accept_button.click()
+
+    # Wait for the next page to load
+    wait.until(EC.url_contains("icpplustieb/acInfo"))
+
+    # Click on the "Entrar" button
+    second_accept_button = wait.until(EC.presence_of_element_located((By.ID, "btnEntrar")))
+    second_accept_button.click()
+
+    # Wait for the next page to load
+    wait.until(EC.url_contains("icpplustieb/acEntrada"))
+
+
+
+
+
     # Optionally, perform any further actions if needed
     # For example, you might want to submit the form
     # submit_button = driver.find_element(By.ID, "submit_button_id")
